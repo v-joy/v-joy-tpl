a9e4607b197a09428739ed042138e8bbd726115d
liujunling03 <liujunling03@baidu.com>
2016-04-20 18:58:12 +0800

    修改build脚本

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # author: v-joy
 # date 2016-04-17
-npm run p
+
+type="${1-jsOnly}";
+
+function uploadfile(){
 scp -r dist root@v-joy.net:/etc/nginx/html/vr/front/
-if [ "$1" == "all" ];then 
+if [ "$type" == "all" ];then 
 scp index.html root@v-joy.net:/etc/nginx/html/vr/front/
 scp -r asset root@v-joy.net:/etc/nginx/html/vr/front/
 fi
+}
+
+
+npm run p
+if [ "$?" == "0" ];then
+uploadfile
+fi
+
